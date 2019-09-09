2d9b4fea6d40c21eee0353d01fabed0d167708d3
wtetsu <tewatana@gmail.com>
2019-09-09 21:13:18 +0900

    Add am e2e test sciprt

---
+++ e2e.sh
@@ -0,0 +1,26 @@
+go build .
+
+mkdir -p .test/abc-def-ghi
+mkdir -p .test/foo-bar-abc
+
+
+CURRNET_DIR=`./sillycd.exe .`
+
+function test() {
+  param=$1
+  expected=$2
+  actual=`./sillycd.exe $1`
+  if [ "$actual" == "$expected" ]; then
+    echo "OK: $param $actual"
+  else
+    echo "ERROR:$param $actual $expected"
+  fi
+}
+
+test . $CURRNET_DIR
+test .. `dirname $CURRNET_DIR`
+test .test/adg `./sillycd .test/abc-def-ghi`
+test .test/f `./sillycd .test/foo-bar-abc`
+test .test/fb `./sillycd .test/foo-bar-abc`
+test .test/fba `./sillycd .test/foo-bar-abc`
+test .test/fbab `./sillycd .test/foo-bar-abc`
